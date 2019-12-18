84d9a2e54c7db084e5c9e3d6de9029fcd0c8a9fa
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-12-18 11:15:28 +0100

    convert some context to global variables

--- beat.c
+++ beat.c
@@ -6,44 +6,46 @@
 #define MIN(A, B) ((A) < (B) ? (A) : (B))
 
 struct context {
-    int samplerate;
-    int frames_per_beat;  // samplerate * 60 / bpm
-    int frames;
-    int buf_cur;
-    size_t buf_len;
-    float factor;
     float *buf;
     float *buf2;
 };
 
-void add_file_at_beat(const char *path, int beat, struct context ctx) {
+int samplerate;
+int frames_per_beat;  // samplerate * 60 / bpm
+int frames;
+int buf_cur;
+size_t buf_len;
+float factor;
+struct context ctx;
+
+void add_file_at_beat(const char *path, int beat) {
     int ibs = 1024;
-    int pos = beat * ctx.frames_per_beat;
+    int pos = beat * frames_per_beat;
     float fbuf[ibs];
     SF_INFO sfinfo;
     SNDFILE *sndfile = sf_open(path, SFM_READ, &sfinfo);
 
-    // assert sfinfo.samplerate == ctx.samplerate
+    // assert sfinfo.samplerate == samplerate
     // assert sfinfo.channels == 1
 
     while (1) {
         int count = sf_readf_float(sndfile, fbuf, ibs);
-        count = MIN(count, ctx.frames - pos - 1);
-        int rel_pos = pos - ctx.buf_cur * ctx.buf_len;
+        count = MIN(count, frames - pos - 1);
+        int rel_pos = pos - buf_cur * buf_len;
 
         if (count <= 0) break;
 
         for (int i = 0; i < count; ++i) {
             pos += 1;
             rel_pos += 1;
-            if (rel_pos >= 2 * ctx.buf_len) {
+            if (rel_pos >= 2 * buf_len) {
                 printf("dropping %s at %i\n", path, pos);
                 sf_close(sndfile);
                 return;
-            } else if (rel_pos >= ctx.buf_len) {
-                ctx.buf2[rel_pos - ctx.buf_len] += fbuf[i] * ctx.factor;
+            } else if (rel_pos >= buf_len) {
+                ctx.buf2[rel_pos - buf_len] += fbuf[i] * factor;
             } else {
-                ctx.buf[rel_pos] += fbuf[i] * ctx.factor;
+                ctx.buf[rel_pos] += fbuf[i] * factor;
             }
         }
     }
@@ -57,29 +59,27 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    struct context ctx;
+    samplerate = atoi(argv[2]);
+    frames_per_beat = samplerate * 60 / atoi(argv[3]);
+    frames = atoi(argv[4]) * frames_per_beat;
+    factor = 1 / sqrt(atoi(argv[5]));
 
-    ctx.samplerate = atoi(argv[2]);
-    ctx.frames_per_beat = ctx.samplerate * 60 / atoi(argv[3]);
-    ctx.frames = atoi(argv[4]) * ctx.frames_per_beat;
-    ctx.factor = 1 / sqrt(atoi(argv[5]));
+    buf_len = MIN(frames / 4, 1 << 18);
+    buf_cur = 0;
 
-    ctx.buf_len = MIN(ctx.frames / 4, 1 << 18);
-    ctx.buf_cur = 0;
-
-    float buf[ctx.buf_len];
-    memset(buf, 0, ctx.buf_len * sizeof(float));
+    float buf[buf_len];
+    memset(buf, 0, buf_len * sizeof(float));
     ctx.buf = buf;
 
-    float buf2[ctx.buf_len];
-    memset(buf2, 0, ctx.buf_len * sizeof(float));
+    float buf2[buf_len];
+    memset(buf2, 0, buf_len * sizeof(float));
     ctx.buf2 = buf2;
 
     SF_INFO sfinfo;
     sfinfo.channels = 1;
     sfinfo.format = SF_FORMAT_FLAC | SF_FORMAT_PCM_16;
-    sfinfo.frames = ctx.frames;
-    sfinfo.samplerate = ctx.samplerate;
+    sfinfo.frames = frames;
+    sfinfo.samplerate = samplerate;
     sfinfo.sections = 1;
     sfinfo.seekable = 1;
 
@@ -89,23 +89,23 @@ int main(int argc, char **argv) {
         int beat = atoi(argv[i]);
         char *path = argv[i + 1];
 
-        if (beat * ctx.frames_per_beat >= (ctx.buf_cur + 1) * ctx.buf_len) {
-            sf_writef_float(sndfile, ctx.buf, ctx.buf_len);
-            memset(ctx.buf, 0, ctx.buf_len * sizeof(float));
+        if (beat * frames_per_beat >= (buf_cur + 1) * buf_len) {
+            sf_writef_float(sndfile, ctx.buf, buf_len);
+            memset(ctx.buf, 0, buf_len * sizeof(float));
 
             float *tmp = ctx.buf;
             ctx.buf = ctx.buf2;
             ctx.buf2 = tmp;
-            ctx.buf_cur += 1;
+            buf_cur += 1;
         }
 
-        add_file_at_beat(path, beat, ctx);
+        add_file_at_beat(path, beat);
     }
 
-    int rest = ctx.frames - ctx.buf_cur * ctx.buf_len;
-    if (rest > ctx.buf_len) {
-        sf_writef_float(sndfile, ctx.buf, ctx.buf_len);
-        sf_writef_float(sndfile, ctx.buf2, rest - ctx.buf_len);
+    int rest = frames - buf_cur * buf_len;
+    if (rest > buf_len) {
+        sf_writef_float(sndfile, ctx.buf, buf_len);
+        sf_writef_float(sndfile, ctx.buf2, rest - buf_len);
     } else {
         sf_writef_float(sndfile, ctx.buf, rest);
     }
